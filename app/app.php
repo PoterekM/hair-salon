e640497eaa07f7830f4810a63bd3cc19d7f75f1d
Michelle Poterek <poterekm@gmail.com>
2017-07-14 12:25:26 -0700

    remove Symfony debugger- was blocking localhost from displaying

--- app/app.php
+++ app/app.php
@@ -7,18 +7,26 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    use Symfony\Componenet\Debug\Debug;
-    Debug::enable();
+    // use Symfony\Componenet\Debug\Debug;
+    // Debug::enable();
 
 
     $app = new Silex\Application();
-    $app['debug'] = true;
+    // $app['debug'] = true;
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() use($app){
+    $app->get("/", function() use($app) {
         return $app['twig']->render('index.html.twig', array(Stylist::getAll()));
+    });
+
+    $app->post("/", function() use ($app) {
+        $stylist = $_POST['stylist'];
+        $new_stylist = new Stylist($stylist);
+
+        $new_stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
 
