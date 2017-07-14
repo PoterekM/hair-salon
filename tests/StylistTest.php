64e4a709e8217f31796f2b1ce5d5a83101c94464
Michelle Poterek <poterekm@gmail.com>
2017-07-14 11:43:12 -0700

    fix bug so get all function passes and fails as it should

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -55,11 +55,11 @@
         }
 
 
-        // protected function tearDown()
-        // {
-        //     Stylist::deleteAll();
-        //     //hey, eventually you'll need one for the clients, too
-        // }
+        protected function tearDown()
+        {
+            Stylist::deleteAll();
+            //hey, eventually you'll need one for the clients, too
+        }
 
         function testSave()
         {
@@ -71,35 +71,35 @@
             $this->assertTrue($executed, "This stylist wasn't saved, bro.");
         }
 
-        // function testGetAll()
-        // {
-        //     $stylist = "Jimi";
-        //     $stylist2 = "Alpert";
-        //     $test_stylist = new Stylist($stylist);
-        //     $test_stylist->save();
-        //     $test_stylist2 = new Stylist($stylist2);
-        //     $test_stylist2->save();
-        //
-        //     $result = Stylist::getAll();
-        //
-        //     $this->assertEquals([$test_stylist, $test_stylist2], $result);
-        // }
-        //
-        // function testDeleteAll()
-        // {
-        //     $stylist = "Bobbo";
-        //     $stylist2 = "Rudy";
-        //     $test_stylist = new Stylist($stylist);
-        //     $test_stylist->save();
-        //     $test_stylist2 = new Stylist($stylist2);
-        //     $test_stylist2->save();
-        //
-        //     Stylist::deleteAll();
-        //     $result = Stylist::getAll();
-        //
-        //     $this->assertEquals([], $result);
-        //
-        // }
+        function testGetAll()
+        {
+            $stylist = "Jimi";
+            $stylist2 = "Alpert";
+            $test_stylist = new Stylist($stylist);
+            $test_stylist->save();
+            $test_stylist2 = new Stylist($stylist2);
+            $test_stylist2->save();
+
+            $result = Stylist::getAll();
+
+            $this->assertEquals([$test_stylist, $test_stylist2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            $stylist = "Bobbo";
+            $stylist2 = "Rudy";
+            $test_stylist = new Stylist($stylist);
+            $test_stylist->save();
+            $test_stylist2 = new Stylist($stylist2);
+            $test_stylist2->save();
+
+            Stylist::deleteAll();
+            $result = Stylist::getAll();
+
+            $this->assertEquals([], $result);
+
+        }
 
     }
 ?>
