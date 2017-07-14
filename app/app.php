5d904849fe7c6a68d95168a073d7b00281bd4bb1
Michelle Poterek <poterekm@gmail.com>
2017-07-14 13:38:07 -0700

    add client to src and test files

--- app/app.php
+++ app/app.php
@@ -1,6 +1,8 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
+
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
@@ -17,17 +19,17 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    // $app->get("/", function() use($app) {
-    //     return $app['twig']->render('index.html.twig', array(Stylist::getAll()));
-    // });
-    //
-    // $app->post("/", function() use ($app) {
-    //     $stylist = $_POST['stylist'];
-    //     $new_stylist = new Stylist($stylist);
-    //
-    //     $new_stylist->save();
-    //     return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
-    // });
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('index.html.twig', array(Stylist::getAll()));
+    });
+
+    $app->post("/", function() use ($app) {
+        $stylist = $_POST['stylist'];
+        $new_stylist = new Stylist($stylist);
+
+        $new_stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
 
 
     // routes
