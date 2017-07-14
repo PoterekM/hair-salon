f4c1232a177553f7bb72a1ff97751a1795fe2227
Michelle Poterek <poterekm@gmail.com>
2017-07-14 08:30:34 -0700

    add basic file structure

---
+++ tests/StylistTest.php
@@ -0,0 +1,29 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+
+        function testName()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+?>
