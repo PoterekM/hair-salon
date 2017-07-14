1fd9a042d1d4c2ebe817de386ed4da12f1c082e5
Michelle Poterek <poterekm@gmail.com>
2017-07-14 14:24:18 -0700

    update database for clients

--- src/Client.php
+++ src/Client.php
@@ -16,5 +16,56 @@
             $this->id = $id;
         }
 
+        function setId()
+        {
+            $this->id = inval($id);
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setName($newName)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setNumber($new_number)
+        {
+            $this->number = intval($new_number)
+        }
+
+        function getDate()
+        {
+            return $this->date;
+        }
+
+        function setDate($new_date)
+        {
+            $this->date = (string) $new_date;
+        }
+
+        function getClientId()
+        {
+            return $this->client_id;
+        }
+
+        function setClientId($new_client_id)
+        {
+            $this->client_id = intval($new_client_id);
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+
     }
 ?>
