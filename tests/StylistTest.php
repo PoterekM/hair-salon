c3c889ceeecf60137d0093b301ea25c5bb2e78d7
Michelle Poterek <poterekm@gmail.com>
2017-07-14 09:12:04 -0700

    first test to get stylist passes and fails as expected

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -7,23 +7,56 @@
 
     require_once "src/Stylist.php";
 
-    $server = 'mysql:host=localhost:8889;dbname=salon_test';
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
 
-    class TaskTest extends PHPUnit_Framework_TestCase
+    class StylistTest extends PHPUnit_Framework_TestCase
     {
 
-        function testName()
+        function testGetStylist()
         {
             //Arrange
+            $stylist = "Nancy";
+            $test_stylist = new Stylist($stylist);
 
             //Act
-
+            $result = $test_stylist->getStylist();
             //Assert
+            $this->assertEquals($stylist, $result);
 
         }
+
+        // function testGetStylist()
+        // {
+        //     //Arrange
+        //
+        //     //Act
+        //
+        //     //Assert
+        //
+        // }
+        //
+        // function testGetStylist()
+        // {
+        //     //Arrange
+        //
+        //     //Act
+        //
+        //     //Assert
+        //
+        // }
+        //
+        // function testGetStylist()
+        // {
+        //     //Arrange
+        //
+        //     //Act
+        //
+        //     //Assert
+        //
+        // }
     }
 ?>
